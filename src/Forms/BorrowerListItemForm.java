24ed922e26ef2371ae1969c608d96d995d1aead8
Nick Izbrodin <nizbrodin@gmail.com>
2023-02-08 20:36:05 +0000

    If return date is in the past then text for that borrower will be red in borrower list.

--- src/Forms/BorrowerListItemForm.java
+++ src/Forms/BorrowerListItemForm.java
@@ -4,6 +4,8 @@ import Entities.Borrower;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import java.awt.*;
+import java.time.LocalDate;
 
 public class BorrowerListItemForm {
     private JLabel borrowerIDLabel;
@@ -15,9 +17,17 @@ public class BorrowerListItemForm {
         return borrowerItemPanel;
     }
 
-    public void setData(Borrower entry) {
+    protected void setData(Borrower entry) {
+
         borrowerIDLabel.setText("ID: "+entry.memberID());
         borrowerFullNameLabel.setText("Full name: "+entry.fullName());
         returnDateLabel.setText("Return date: "+entry.returnDate());
+
+        LocalDate currentDate = java.time.LocalDate.now();
+        if (entry.returnDate().isBefore(currentDate) || entry.returnDate().equals(currentDate)) {
+            borrowerIDLabel.setForeground(Color.RED);
+            borrowerFullNameLabel.setForeground(Color.RED);
+            returnDateLabel.setForeground(Color.RED);
+        }
     }
 }
